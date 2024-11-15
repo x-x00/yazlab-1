260effe6f11e0f74a51b4f021c1203d8c1fc1707
x-x00 <154417035+x-x00@users.noreply.github.com>
2024-11-15 12:24:12 +0300

    Add files via upload

--- visualize-mfcc.py
+++ visualize-mfcc.py
@@ -4,19 +4,26 @@ import librosa.display
 import pandas as pd
 
 def visualize_mfcc(mfcc_file_path):
-    # Load the MFCC features from the .csv file
+    """
+    Visualizes MFCC feature converted from csv file using pyplot.
+    
+    Parameters:
+    - mfcc_file_path: Path to the csv file that contains MFCC features.
+    """
+    
+    # Load the MFCC features from the csv file
     mfcc_df = pd.read_csv(mfcc_file_path)
 
-    # Convert DataFrame to NumPy array
+    # Convert dataframe to numpy array
     mfcc = mfcc_df.to_numpy()
     
-    # Plot the MFCC features
+    # Plot the MFCC features ( time axis should be 5 seconds but instead it's less than that ? )
     plt.figure(figsize=(10, 6))
     librosa.display.specshow(mfcc, x_axis='time')
     plt.colorbar(format='%+2.0f dB')
     plt.title('MFCC')
     plt.xlabel('Time (s)')
-    plt.ylabel('MFCC Coefficients')
+    plt.ylabel('MFCC')
     plt.tight_layout()
     plt.show()
 
@@ -25,7 +32,7 @@ def visualize_mfcc(mfcc_file_path):
     # plt.colorbar(label='Amplitude (dB)')
     # plt.title('MFCC')
     # plt.xlabel('Time')
-    # plt.ylabel('MFCC Coefficients')
+    # plt.ylabel('MFCC')
     # plt.tight_layout()
     # plt.show()
 
